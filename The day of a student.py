08e6baa96af1a5b9db8fa535e09f2ba69cb9fbae
poppot <148318687+poppot@users.noreply.github.com>
2024-03-25 09:02:43 +0800

    Delete The day of a student.py

--- The day of a student.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-dict={"Sleep":8,"Classes":6,"Study":3.5,"TV":2,"Music":1,'Exercise':3.5}    #input 'other' here
-time=[8,6,3.5,2,1,3.5]
-events=dict.keys()
-print(dict)
-plt.figure
-plt.pie(time,labels=events)
-plt.show()
-plt.clf()
