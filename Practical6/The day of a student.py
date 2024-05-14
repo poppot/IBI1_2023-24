dba5781e8351e72ed02096a766313475cbde47fd
poppot <148318687+poppot@users.noreply.github.com>
2024-05-14 16:01:16 +0800

    Add files via upload

--- Practical6/The day of a student.py
+++ Practical6/The day of a student.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 dict={"Sleep":8,"Classes":6,"Study":3.5,"TV":2,"Music":1,'Exercise':3.5}    #input 'other' here
 time=[8,6,3.5,2,1,3.5]
-events=dict.keys()
+events=dict.keys()                                                          #put the events into a list 
 print(dict)
 plt.figure
-plt.pie(time,labels=events)
+plt.pie(time,labels=events)                                                #draw the figure
 plt.show()
 plt.clf()
--- Practical6/city size.py
+++ Practical6/city size.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
-plt.rcParams['figure.figsize']=[8,6]
-uk_cities=[0.56,0.62,0.04,9.7]
-ch_cities=[0.58,8.4,29.9,22.2]
+plt.rcParams['figure.figsize']=[8,6]                       #set figure size
+uk_cities=[0.56,0.62,0.04,9.7]                             #input the y value
+ch_cities=[0.58,8.4,29.9,22.2]                             
 uk_cities.sort()
-ch_cities.sort()
+ch_cities.sort()                                          #sort the cities
 print(uk_cities)
 print(ch_cities)
-x1=['Stirling','Edinburgh','Glasgow','London']
+x1=['Stirling','Edinburgh','Glasgow','London']            #iput the x value
 x2=['Haining','Hangzhou','Shanghai','Beijing']
 plt.figure()
-ax1=plt.subplot(221)
+ax1=plt.subplot(221)                                     #draw two figure in one picture
 plt.bar(x1,uk_cities)
 plt.ylabel("population")
 plt.xlabel('UK cities')
@@ -18,6 +18,6 @@ ax2=plt.subplot(222)
 plt.bar(x2,ch_cities)
 plt.ylabel("population")
 plt.xlabel('Chinese cities')
-plt.title('population of Chinese cities')
+plt.title('population of Chinese cities')                    #title
 plt.show()
 plt.clf
