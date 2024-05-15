72f3d074a828d478875648761e58caae761eee74
poppot <148318687+poppot@users.noreply.github.com>
2024-05-15 09:47:36 +0800

    Add files via upload

--- Practical8/count_repeat_duplicate_genes.py
+++ Practical8/count_repeat_duplicate_genes.py
@@ -5,12 +5,12 @@ genes=(open('duplicate_genes.fa','r',encoding='UTF-8')).read()                  
 repeat_genes=str(input('input the repeat genes:'))                                         #receive the repeat genes
 gene_name=re.findall(r'Y......',genes)                                                     #find the name of genes
 genes=re.sub(r'\n','',genes)                                                               #delete all line break
-genes=re.sub(r'Y......','\n',genes)                                                        #replace gene name with line break to split sequences with line break
+genes=re.sub(r'>Y......','\n',genes)                                                        #replace gene name with line break to split sequences with line break
 index=r'{}(?={})'.format(repeat_genes[0],repeat_genes[1:])                                 
 roll=0
 content=''
-for i in genes.splitlines():                                                               #read the sequences line by line
-    if i!='':                                                                              #the first line is \n
+for i in genes.splitlines():                                                               #read the sequences line by line  
+    if i!='':                                                                               #the first line is \n
         list1=re.findall(index,i)                                                          #find repeat sequence
         content=content+'>'+gene_name[roll]+' (the times of repetition:'+str(len(list1))+')'+'\n'+i+'\n'    
         roll=roll+1
